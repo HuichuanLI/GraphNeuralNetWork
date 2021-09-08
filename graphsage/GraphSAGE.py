a818cbca3ee3d646fbbe04c6ae8cd4058abfbaeb
huichuan <lhc14124908@163.com>
2021-09-08 23:21:36 +0800

    add

--- graphsage/GraphSAGE.py
+++ graphsage/GraphSAGE.py
@@ -14,7 +14,7 @@ from tensorflow.python.keras.regularizers import l2
 
 class MeanAggregator(Layer):
     def __init__(self, units, input_dim, neigh_max, concat=True, dropout_rate=0.0, activation=tf.nn.relu, l2_reg=0,
-                 use_bias=False,
+                 use_bias=False, gcn=True,
                  seed=1024, **kwargs):
         super(MeanAggregator, self).__init__()
         self.units = units
@@ -26,13 +26,22 @@ class MeanAggregator(Layer):
         self.activation = activation
         self.seed = seed
         self.input_dim = input_dim
+        self.gcn = gcn
 
     def build(self, input_shapes):
-        self.neigh_weights = self.add_weight(shape=(self.input_dim, self.units),
-                                             initializer=glorot_uniform(
-                                                 seed=self.seed),
-                                             regularizer=l2(self.l2_reg),
-                                             name="neigh_weights")
+        if self.gcn:
+            self.neigh_weights = self.add_weight(shape=(self.input_dim * 2, self.units),
+                                                 initializer=glorot_uniform(
+                                                     seed=self.seed),
+                                                 regularizer=l2(self.l2_reg),
+                                                 name="neigh_weights")
+
+        else:
+            self.neigh_weights = self.add_weight(shape=(self.input_dim, self.units),
+                                                 initializer=glorot_uniform(
+                                                     seed=self.seed),
+                                                 regularizer=l2(self.l2_reg),
+                                                 name="neigh_weights")
         if self.use_bias:
             self.bias = self.add_weight(shape=(self.units), initializer=Zeros(),
                                         name='bias_weight')
@@ -44,14 +53,19 @@ class MeanAggregator(Layer):
 
         node_feat = tf.nn.embedding_lookup(features, node)
         neigh_feat = tf.nn.embedding_lookup(features, neighbours)
+        print(node_feat)
+        print(neigh_feat)
 
         node_feat = self.dropout(node_feat, training=training)
         neigh_feat = self.dropout(neigh_feat, training=training)
+        if self.gcn:
+            concat_mean = tf.reduce_mean(neigh_feat, axis=1, keepdims=False)
+            concat_feat = tf.concat([tf.squeeze(node_feat), concat_mean], axis=1)
+            output = tf.matmul(concat_feat, self.neigh_weights)
+        else:
+            concat_mean = tf.reduce_mean(neigh_feat, axis=1, keepdims=False)
+            output = tf.matmul(concat_mean, self.neigh_weights)
 
-        concat_feat = tf.concat([neigh_feat, node_feat], axis=1)
-        concat_mean = tf.reduce_mean(concat_feat, axis=1, keep_dims=False)
-
-        output = tf.matmul(concat_mean, self.neigh_weights)
         if self.use_bias:
             output += self.bias
         if self.activation:
@@ -76,5 +90,20 @@ def GraphSAGE(feature_dim, neighbor_num, n_hidden, n_classes, use_bias=True, act
     neighbor_input = [Input(shape=(l,), dtype=tf.int32) for l in neighbor_num]
     if aggregator_type == 'mean':
         aggregator = MeanAggregator
-
     h = features
+
+    for i in range(0, len(neighbor_num)):
+        if i > 0:
+            feature_dim = n_hidden
+        if i == len(neighbor_num) - 1:
+            activation = tf.nn.softmax
+            n_hidden = n_classes
+        h = aggregator(units=n_hidden, input_dim=feature_dim, activation=activation, l2_reg=l2_reg, use_bias=use_bias,
+                       dropout_rate=dropout_rate, neigh_max=neighbor_num[i], aggregator=aggregator_type)(
+            [h, node_input, neighbor_input[i]])
+    output = h
+    input_list = [features, node_input] + neighbor_input
+    model = Model(input_list, outputs=output)
+    model.__setattr__("embedding", output)
+
+    return model
--- graphsage/main.py
+++ graphsage/main.py
@@ -10,20 +10,25 @@ from tensorflow.keras.optimizers import Adam
 from tensorflow.python.keras.layers import Lambda
 from tensorflow.python.keras.models import Model
 from utils import load_data, sample_neighs
-
+from GraphSAGE import GraphSAGE
 
 if __name__ == "__main__":
     adj, features, y_train, y_val, y_test, train_mask, val_mask, test_mask = load_data(path="../Graph/data/cora/")
     indexs = np.arange(adj.shape[0])
     neigh_number = [10, 25]
     neigh_maxlen = []
+    print(features.shape[1])
 
-    model_input = [features, np.asarray(indexs, dtype=np.int32)]
+    model_input = [features.toarray(), np.asarray(indexs, dtype=np.int32)]
+    print(model_input)
     for num in neigh_number:
         sample_neigh, sample_neigh_len = sample_neighs(
-            adj, indexs, num, self_loop=False)
+            adj.toarray(), indexs, num, self_loop=False)
+        print(sample_neigh.shape)
         model_input.extend([sample_neigh])
         neigh_maxlen.append(max(sample_neigh_len))
+
+
     model = GraphSAGE(feature_dim=features.shape[1],
                       neighbor_num=neigh_maxlen,
                       n_hidden=16,
@@ -34,3 +39,13 @@ if __name__ == "__main__":
                       dropout_rate=0.5, l2_reg=2.5e-4)
     model.compile(Adam(0.01), 'categorical_crossentropy',
                   weighted_metrics=['categorical_crossentropy', 'acc'])
+    print("start training")
+
+    val_data = (model_input, y_val, val_mask)
+
+    model.fit(model_input, y_train, sample_weight=train_mask, validation_data=val_data,
+              batch_size=adj.shape[0], epochs=1, shuffle=False, verbose=2)
+
+    embedding_model = Model(model.input, model.embedding)
+    embedding_weights = embedding_model.predict(model_input, batch_size=adj.shape[0])
+
