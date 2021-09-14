2cc020ac17f39a74d0c9b9042c7144bf004e9314
huichuan <lhc14124908@163.com>
2021-09-14 23:37:24 +0800

    aff

---
+++ GGNN/utils/test.py
@@ -0,0 +1,34 @@
+import torch
+from torch.autograd import Variable
+
+def test(dataloader, net, criterion, optimizer, opt):
+    test_loss = 0
+    correct = 0
+    net.eval()
+    for i, (adj_matrix, annotation, target) in enumerate(dataloader, 0):
+        padding = torch.zeros(len(annotation), opt.n_node, opt.state_dim - opt.annotation_dim).double()
+        init_input = torch.cat((annotation, padding), 2)
+        if opt.cuda:
+            init_input = init_input.cuda()
+            adj_matrix = adj_matrix.cuda()
+            annotation = annotation.cuda()
+            target = target.cuda()
+
+        init_input = Variable(init_input)
+        adj_matrix = Variable(adj_matrix)
+        annotation = Variable(annotation)
+        target = Variable(target)
+
+        output = net(init_input, annotation, adj_matrix)
+
+        #python2
+        #test_loss += criterion(output, target).data[0]
+        test_loss += criterion(output, target).data
+        pred = output.data.max(1, keepdim=True)[1]
+
+        correct += pred.eq(target.data.view_as(pred)).cpu().sum()
+
+    test_loss /= len(dataloader.dataset)
+    print('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)'.format(
+        test_loss, correct, len(dataloader.dataset),
+        100. * correct / len(dataloader.dataset)))
---
+++ GGNN/utils/train.py
@@ -0,0 +1,30 @@
+import torch
+from torch.autograd import Variable
+
+def train(epoch, dataloader, net, criterion, optimizer, opt):
+    net.train()
+    for i, (adj_matrix, annotation, target) in enumerate(dataloader, 0):
+        net.zero_grad()
+
+        padding = torch.zeros(len(annotation), opt.n_node, opt.state_dim - opt.annotation_dim).double()
+        init_input = torch.cat((annotation, padding), 2)
+        if opt.cuda:
+            init_input = init_input.cuda()
+            adj_matrix = adj_matrix.cuda()
+            annotation = annotation.cuda()
+            target = target.cuda()
+
+        init_input = Variable(init_input)
+        adj_matrix = Variable(adj_matrix)
+        annotation = Variable(annotation)
+        target = Variable(target)
+
+        output = net(init_input, annotation, adj_matrix)
+
+        loss = criterion(output, target)
+
+        loss.backward()
+        optimizer.step()
+
+        if i % int(len(dataloader) / 10 + 1) == 0 and opt.verbal:
+            print('[%d/%d][%d/%d] Loss: %.4f' % (epoch, opt.niter, i, len(dataloader), loss.data[0]))
